c0dd068ae0e28018ea204bb4da6baa18c9a1f21d
adibcoding <127365003+adibcoding@users.noreply.github.com>
2026-05-17 22:51:08 +0800

    feature: init project and created sidebar with components slicing (#1)

---
+++ src/app/fonts.ts
@@ -0,0 +1,15 @@
+import { Inter, Noto_Serif } from 'next/font/google'
+
+export const inter = Inter(
+    {
+        display: 'swap',
+        variable: '--font-inter'
+    }
+)
+
+export const notoSerif = Noto_Serif(
+    {
+        display: 'swap',
+        variable: '--font-noto-serif'
+    }
+)
